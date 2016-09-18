8d21448f73d429e8e3975b8b53422aab6cab21e2
injetlee <liyingjie26@hotmail.com>
2016-09-18 20:38:05 +0800

    improve index

--- zhihu/urls.py
+++ zhihu/urls.py
@@ -4,9 +4,9 @@ from zhihu import views
 app_name = 'zhihu'
 urlpatterns = [
     url(r'^$', views.login, name='login'),
-    url(r'^login/', views.login, name='login'),
-    url(r'^reg/', views.reg, name='reg'),
-    url(r'^index/', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^reg/$', views.reg, name='reg'),
+    url(r'^index/$', views.index, name='index'),
     url(r'^active/(?P<token>(.*))/$', views.active, name='active'),
     url(r'^logout_view/$', views.logout_view, name='logout_view')
 ]
